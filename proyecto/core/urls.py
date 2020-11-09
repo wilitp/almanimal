394d71a858387464d12e7e2404b91b45cb8c6d3e
ebnerlautaro <lautiebner@gmail.com>
2020-11-09 11:35:37 -0300

    borro pagina nosotros

--- proyecto/core/urls.py
+++ proyecto/core/urls.py
@@ -21,7 +21,6 @@ from . import views
 urlpatterns = [
 
     path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
     path('contacto/', views.contacto, name='contacto'),
     path('donaciones/', views.donaciones, name='donaciones'),
 ]
