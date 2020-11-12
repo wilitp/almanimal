01159f200c98391a8c5471fbe527c601e4d8fe9e
juampiludu <juanzakka@gmail.com>
2020-11-12 11:32:16 -0300

    validacion de nombre de usuario en registro agregado

--- proyecto/usuario/views.py
+++ proyecto/usuario/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from .forms import RegistroForm
+from django.contrib import messages
 
 def logIn(request):
 
@@ -37,9 +38,13 @@ def registro(request):
     if request.method == 'POST':
 
         form = RegistroForm(request.POST)
+        
         if form.is_valid():
             user = form.save()
             return redirect('iniciar-sesion')
+        
+        elif User.objects.filter(username=request.POST.get('username')).exists():
+            messages.error(request, 'Este nombre de usuario no está disponible.')
 
     return render(request, 'register.html', {'form' : form})
 
