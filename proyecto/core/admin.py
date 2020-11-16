e30ec487e801a3d0d0d190d23db8ae483700bed7
ebnerlautaro <lautiebner@gmail.com>
2020-11-16 13:49:38 -0300

    creo modelos para manejar los contenidos de las paginas inicio y donaciones

--- proyecto/core/admin.py
+++ proyecto/core/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Contact
+from .models import Contact, PaginaInicio, PaginaDonaciones
 
 # Register your models here.
 
-admin.site.register(Contact)
+# admin.site.register(Contact)
+admin.site.register(PaginaInicio)
+admin.site.register(PaginaDonaciones)
