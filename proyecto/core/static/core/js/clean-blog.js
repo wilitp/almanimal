c51481d9090c6492d6d2e82b56765c9a0211d891
juampiludu <juanzakka@gmail.com>
2020-11-19 17:43:39 -0300

    tema oscuro de logo en dispositivos moviles agregado

--- proyecto/core/static/core/js/clean-blog.js
+++ proyecto/core/static/core/js/clean-blog.js
@@ -12,10 +12,10 @@
 
   // Show the navbar when the page is scrolled up
   var MQL = 992;
-  $('#dark-logo').hide();
 
   //primary navigation slide-in effect
   if ($(window).width() > MQL) {
+    $('#dark-logo').hide();
     var headerHeight = $('#mainNav').height();
     $(window).on('scroll', {
         previousTop: 0
@@ -33,6 +33,31 @@
             $('#mainNav').removeClass('is-visible is-fixed');
             $('#light-logo').show();
             $('#dark-logo').hide();
+          }
+        } else if (currentTop > this.previousTop) {
+          //if scrolling down...
+          $('#mainNav').removeClass('is-visible');
+          if (currentTop > headerHeight && !$('#mainNav').hasClass('is-fixed')) $('#mainNav').addClass('is-fixed');
+        }
+        this.previousTop = currentTop;
+      });
+  }
+  else {
+    $('#dark-logo').show();
+    $('#light-logo').hide();
+    var headerHeight = $('#mainNav').height();
+    $(window).on('scroll', {
+        previousTop: 0
+      },
+      function() {
+        var currentTop = $(window).scrollTop();
+        //check if user is scrolling up
+        if (currentTop < this.previousTop) {
+          //if scrolling up...
+          if (currentTop > 0 && $('#mainNav').hasClass('is-fixed')) {
+            $('#mainNav').addClass('is-visible');
+          } else {
+            $('#mainNav').removeClass('is-visible is-fixed');
           }
         } else if (currentTop > this.previousTop) {
           //if scrolling down...
